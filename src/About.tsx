f8578ece3ea17a39d8feb5f5f4d300b112a6133b
abhisarshukla <abhisarshukla645@gmail.com>
2021-02-03 11:25:13 +0530

    Remove Unwanted Stuff

--- src/About.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FunctionComponent } from "react";
-import { RouteComponentProps } from "@reach/router";
-
-const About: FunctionComponent<RouteComponentProps> = () => {
-  return <h1>About</h1>;
-};
-
-export default About;
--- src/Submit.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FunctionComponent } from "react";
-import { RouteComponentProps } from "@reach/router";
-
-const Submit: FunctionComponent<RouteComponentProps> = () => {
-  return <h1>Submit</h1>;
-};
-
-export default Submit;
